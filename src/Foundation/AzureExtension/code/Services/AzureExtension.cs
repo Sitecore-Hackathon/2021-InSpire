7c308fe71c32cbd7ad6de77e5aee9c1df94cdbdf
sitecorelibraries <pradeepgupta.nitrr@gmail.com>
2021-03-06 17:13:58 +0530

    azure extension code updates

---
+++ src/Foundation/AzureExtension/code/Services/AzureExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System.Configuration;
+
+namespace SCHackathon.Foundation.AzureExtension.Services
+{
+    public class AzureExtension
+    {
+        public async System.Threading.Tasks.Task azStorageQueueConAsync(string body, string AzureQueueName)
+        {
+            var azQueueAcName = ConfigurationManager.AppSettings["AzureStorageAccountName"];
+            var azQueueAcKey = ConfigurationManager.AppSettings["AzureStorageAccountkey"];
+            var saCred = new StorageCredentials(azQueueAcName, azQueueAcKey);
+            var saConfig = new CloudStorageAccount(saCred, true);
+            var azClient = saConfig.CreateCloudQueueClient();
+            var azQueue = azClient.GetQueueReference(AzureQueueName);
+            await azQueue.CreateIfNotExistsAsync();
+            var msgToSend = new CloudQueueMessage(body);
+            await azQueue.AddMessageAsync(msgToSend);
+        }
+    }
+}
