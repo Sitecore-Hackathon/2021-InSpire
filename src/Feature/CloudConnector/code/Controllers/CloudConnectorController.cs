194c24aba0c1a71c1943fd449754a5e0f4f98814
mohithackathon2020 <hackathon@2020>
2021-03-06 12:53:55 +0000

    #Push for Azure Connectivity feature

---
+++ src/Feature/CloudConnector/code/Controllers/CloudConnectorController.cs
@@ -0,0 +1,54 @@
+﻿using Azure.Identity;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+using SCHackathon.Feature.CloudConnector.Models;
+using Sitecore.Diagnostics;
+using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Web;
+
+namespace SCHackathon.Feature.CloudConnector.Controllers
+{
+    public class CloudConnectorController : Controller
+    {
+        [HttpGet]
+        public JsonResult ConnectToAzure(AzureProperties azureProperties)
+        {
+            bool result = false;
+            try
+            {
+                try
+                {
+                    var azQueueAcName = azureProperties.AzureAccountName;
+                    var azQueueAcKey = azureProperties.SecretKey;
+                    var saCred = new StorageCredentials(azQueueAcName, azQueueAcKey);
+                    var saConfig = new CloudStorageAccount(saCred, true);
+                    var azClient = saConfig.CreateCloudQueueClient();
+                    if (azClient.BaseUri != null)
+                    {
+                        this.Session.Add("AzClientData", azClient);
+                        result = true;
+                        return Json(result, JsonRequestBehavior.AllowGet);
+                    }
+                }
+                catch (AuthenticationFailedException e)
+                {
+                    Console.WriteLine($"Authentication Failed. {e.Message}");
+                }
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.StackTrace, ex, typeof(CloudConnectorController));
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+    }
+}
---
+++ src/Feature/CloudConnector/code/Models/AzureProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCHackathon.Feature.CloudConnector.Models
+{
+    public class AzureProperties
+    {
+        public string AzureAccountName { get; set; }
+        public string SecretKey { get; set; }
+        public string AzureUrl { get; set; }
+        public string AzureQueueName { get; set; }
+    }
+}
