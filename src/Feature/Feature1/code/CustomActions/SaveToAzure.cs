c73c6e0609ced3e1e155c68999b5b6d7e9b252af
sitecorelibraries <pradeepgupta.nitrr@gmail.com>
2021-03-06 14:15:48 +0530

    Form Feature Code update

---
+++ src/Feature/Feature1/code/CustomActions/SaveToAzure.cs
@@ -0,0 +1,121 @@
+﻿using Newtonsoft.Json;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Mvc.Models.Fields;
+using Sitecore.ExperienceForms.Processing;
+using Sitecore.ExperienceForms.Processing.Actions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SCHackathon.Feature.Form.CustomActions
+{
+    public class SaveToAzure : SubmitActionBase<FormDataModel>
+    {
+        public SaveToAzure(ISubmitActionData submitActionData) : base(submitActionData)
+        {
+        }
+        /// <param name="data"></param>
+        /// <param name="formSubmitContext"></param>
+        /// <returns></returns>
+        protected override bool Execute(FormDataModel data, FormSubmitContext formSubmitContext)
+        {
+            Assert.ArgumentNotNull(formSubmitContext, nameof(formSubmitContext));
+            
+            try
+            {
+                var key = string.Empty;
+                var value = string.Empty;
+                var formFieldName = string.Empty;
+                var dict = new Dictionary<string, string>();
+                var fields = formSubmitContext.Fields.ToList();
+                foreach(var item in fields)
+                {
+                    formFieldName = item.Name;
+                    value = GetFieldValue(formSubmitContext, formFieldName);
+                    dict.Add(formFieldName, value);
+                }
+                var dictionary = new Dictionary<string, object>
+                {
+                    {"FormData", dict}
+                };
+                var formDetails = JsonConvert.SerializeObject(dictionary);
+                //this.SendMessage(formDetails);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Error sending data to azure", e, this);
+                //formSubmitContext.Abort(); optional
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// To get Field values
+        /// </summary>
+        /// <param name="formSubmitContext"></param>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        private string GetFieldValue(FormSubmitContext formSubmitContext, string fieldName)
+        {
+            var fieldValue = string.Empty;
+            try
+            {
+                var valueField = formSubmitContext.Fields.FirstOrDefault(f => f.Name.Equals(fieldName));
+                var property = valueField?.GetType().GetProperty("Value");
+                if (property != null)
+                {
+                    var postedId = property.GetValue(valueField);
+
+                    if (valueField.GetType().Name == "DropDownListViewModel")
+                    {
+                        var result = ((IEnumerable)postedId).Cast<object>().ToList();
+                        fieldValue = result.FirstOrDefault()?.ToString();
+                    }
+                    else if (valueField.GetType().Name == "ListViewModel")
+                    {
+                        var listField = (ListViewModel)postedId;
+                        var array = listField?.Value?.ToArray();
+                        if (array == null)
+                        {
+                            return string.Empty;
+                        }
+                        return String.Join(",", array);
+                    }
+                    else if (valueField.GetType().Name == "DateViewModel")
+                    {
+                        var dateField = (DateViewModel)postedId;
+                        return dateField.Value.HasValue ? dateField.Value.Value.ToShortDateString() : string.Empty;
+                    }
+                    else if (valueField.GetType().Name == "NumberViewModel")
+                    {
+                        var numberField = (NumberViewModel)postedId;
+                        return numberField.Value.HasValue ? numberField.Value.ToString() : string.Empty;
+                    }
+                    else if (valueField.GetType().Name == "CheckBoxListViewModel")
+                    {
+                        var checkbox = ((IEnumerable)postedId).Cast<object>().ToList();
+                        var array = checkbox.ToArray();
+                        return String.Join(",", array);
+                    }
+                    else if (valueField.GetType().Name == "TextViewModel")
+                    {
+                        var textField = (TextViewModel)postedId;
+                        return (string)(object)textField.Text;
+                    }
+                    else
+                    {
+                        fieldValue = postedId.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Error Get FieldValue for {fieldName}", ex, this);
+            }
+            return fieldValue;
+        }
+    }
+}
---
+++ src/Feature/Feature1/code/Models/FormDataModel.cs
@@ -0,0 +1,13 @@
+﻿using Sitecore.ExperienceForms.Processing.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCHackathon.Feature.Form.Models
+{
+    public class FormDataModel
+    {
+
+    }
+}
